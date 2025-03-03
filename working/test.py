7ad122ce30188ccab597a21914d7ef99edfcea72
Sofia Dossena <sofiadossena@Air-di-Sofia.home-life.hub>
2025-03-03 19:44:55 +0100

    primo file

---
+++ working/test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  3 19:36:23 2025
+
+@author: sofiadossena
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy.io import fits
+
+
+print ("Hello world")
