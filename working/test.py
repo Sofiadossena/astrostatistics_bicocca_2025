7ba8893a669e5498ca0bc2d5fd18eb4546d6568a
Sofiadossena <sofydosse@gmail.com>
2025-03-20 21:09:52 +0100

    ex

---
+++ working/test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  3 19:36:23 2025
+
+@author: sofiadossena
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy.io import fits
+
+
+print ("Hello world")
+print("proviamo a fare qualche modifica ")
+
+#ok quindi l'ordine è add, commit e push
